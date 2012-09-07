7768b805334f7db41e0d629764dae48074f0951e
imodify.git <imodify.git@gmai.com>
2012-09-07 16:30:02 +0530

    item-47,48 event and article

--- themes/imodify/templates/article.tpl.php
+++ themes/imodify/templates/article.tpl.php
@@ -1,62 +1,15 @@
-<?php /*<script type="text/javascript" language="javascript">
-	$(document).ajaxComplete(function(){
-		if($('.selectList #hierarchical-select-1-wrapper div.selects select:first-child').next('span').attr('class')!='customStyleSelectBox'){
-			$('.selectList #hierarchical-select-1-wrapper div.selects select:first-child').customSelect();
-			$(document).ajaxComplete(function(){
-				if($('.selectList #hierarchical-select-1-wrapper div.selects select:last-child').next('span').attr('class')!='customStyleSelectBox'){
-					$('.selectList #hierarchical-select-1-wrapper div.selects select:last-child').customSelect();
-				}
-			});
-		}
-      });
-</script>*/?>
-<div class="<?php echo $classes;?>" id="carStatusEditForm">
-  <div class="tophead1"><?php echo arg(1)=='add'?'Add An ':'Edit ';?>Article</div>
-  <div class="updatecar_main">
-	<div class="updatecar_box1">
-            <div class="updatecar_box1_leftmain">
-              <table width="274" border="0" cellspacing="0" cellpadding="0">
-                <tr>
-                  <td><?php print drupal_render_children($form['title']);?></td>
-                </tr>
-                <tr>
-                  <td class="selectList artSelect"><?php print drupal_render_children($form['field_article_category']);?></td>
-                </tr>
-                <tr>
-                  <td class="selectList"><?php print drupal_render_children($form['field_car_make']);?></td>
-                </tr>
-				<tr>
-                  <td class="selectList artSelect"><?php print drupal_render_children($form['field_supcategory']);?></td>
-                </tr>
-              </table>
-            </div>
-            <div class="updatecar_box1_rightmain" style="margin:0px 0 0 10px;">
-              <table width="234" border="0" cellspacing="0" cellpadding="0">
-                <tr>
-                  <td class="artTagMod" height="20"><?php print drupal_render_children($form['field_article_tags']);?></td>
-                </tr>
-                <tr>
-                  <td class="artTagMod" height="35"><?php print drupal_render_children($form['field_car_model']);?></td>
-                </tr>
-                <tr>
-                  <td class="multiSelect"><?php print drupal_render_children($form['field_car_style']);?> <span style="float:right; font-size:9px;">Select multiple styles</span></td>
-                </tr>
-              </table>
-            </div>
-        </div>
-	  <table width="514" border="0" cellpadding="0" cellspacing="0" align="right">
-		<tbody style="border:none;">
-		<tr>
-		  <td style="padding-bottom:10px;"><?php print drupal_render_children($form['field_article_image']); ?></td>
-		</tr>
-		<tr>
-		  <td><?php print drupal_render_children($form['body']); ?></td>
-		</tr>
-		<tr>
-		  <td align="center" height="50" class="artAct"><?php print drupal_render_children($form['actions']); ?></td>
-		</tr>
-		</tbody>
-	  </table>
-  </div>
-</div>
-<div style="display:none"><?php print drupal_render_children($form); ?></div>
+<?php print drupal_render_children($form); ?>
+<script type="text/javascript" language="javascript">
+	$(document).ready(function(){
+		$("#overlay").css('min-width','800px')
+		$("#overlay").css('width','800px');
+		$("#overlay #content").css('width','100%');
+		$("#overlay #content").css('margin','0');
+		$("#overlay-content").css('-moz-border-top-left-radius','15px');
+		$("#overlay-content").css('border-top-left-radius','15px');
+		$("#overlay-content").css('-moz-border-bottom-left-radius','15px');
+		$("#overlay-content").css('border-bottom-left-radius','15px');
+		$("#overlay-content").css('-moz-border-bottom-right-radius','15px');
+		$("#overlay-content").css('border-bottom-right-radius','15px');
+	});
+</script>
--- themes/imodify/templates/event.tpl.php
+++ themes/imodify/templates/event.tpl.php
@@ -1,64 +1,15 @@
-<div class="<?php echo $classes;?>" id="carStatusEditForm">
-  <div class="tophead1"><?php echo arg(1)=='add'?'Add An ':'Edit ';?>Event</div>
-  <div class="updatecar_main">
-	<div class="updatecar_box1">
-            <div class="updatecar_box1_leftmain">
-              <table width="274" border="0" cellspacing="0" cellpadding="0">
-                <tr>
-                  <td><?php print drupal_render_children($form['title']);?></td>
-                </tr>
-                <tr>
-                  <td class="selectList"><?php print drupal_render_children($form['field_event_category']);?></td>
-                </tr>
-                <tr>
-                  <td class="selectList"><?php print drupal_render_children($form['field_car_make']);?></td>
-                </tr>
-				<tr>
-                  <td class="selectList"><?php print drupal_render_children($form['field_supcategory']);?></td>
-                </tr>
-				<tr>
-                  <td class="selectList"><?php print drupal_render_children($form['taxonomy_vocabulary_1']);?></td>
-                </tr>
-				 <tr>
-                  <td class="artTagMod"><?php print drupal_render_children($form['field_car_model']);?></td>
-                </tr>
-				<tr>
-                  <td class="artTagMod"><?php print drupal_render_children($form['field_link']);?></td>
-                </tr>
-              </table>
-            </div>
-            <div class="updatecar_box1_rightmain" style="margin:0px 0 0 10px;">
-              <table width="234" border="0" cellspacing="0" cellpadding="0">
-                <tr>
-                  <td class="artTagMod"><?php print drupal_render_children($form['field_event_date']);?></td>
-                </tr>
-                <tr>
-                  <td class="multiSelect" style="padding-top:10px;"><?php print drupal_render_children($form['field_car_style']);?> <span style="float:right; font-size:9px;">Select multiple styles</span></td>
-                </tr>
-              </table>
-            </div>
-        </div>
-	  <table width="514" border="0" cellpadding="0" cellspacing="0" align="right">
-		<tbody style="border:none;">
-		<tr>
-		  <td><?php print drupal_render_children($form['field_event_image']); ?></td>
-		</tr>
-		<tr>
-		  <td><?php print drupal_render_children($form['field_event_desc']); ?></td>
-		</tr>
-		<tr>
-		  <td align="center" height="50" class="artAct"><?php print drupal_render_children($form['actions']); ?></td>
-		</tr>
-		</tbody>
-	  </table>
-  </div>
-</div>
-<div style="display:none"><?php print drupal_render_children($form); ?></div>
+<?php print drupal_render_children($form); ?>
 <script type="text/javascript" language="javascript">
-	/*$(document).ready(function(){
-		$("div.form-item-field-event-desc-und-0-format").addClass('selectList');
-		if($("div.form-item-field-event-desc-und-0-format").next('span').attr('class')!='customStyleSelectBox'){
-        	$("div.form-item-field-event-desc-und-0-format").customSelect();
-		}
-	});*/
+	$(document).ready(function(){
+		$("#overlay").css('min-width','800px')
+		$("#overlay").css('width','800px');
+		$("#overlay #content").css('width','100%');
+		$("#overlay #content").css('margin','0');
+		$("#overlay-content").css('-moz-border-top-left-radius','15px');
+		$("#overlay-content").css('border-top-left-radius','15px');
+		$("#overlay-content").css('-moz-border-bottom-left-radius','15px');
+		$("#overlay-content").css('border-bottom-left-radius','15px');
+		$("#overlay-content").css('-moz-border-bottom-right-radius','15px');
+		$("#overlay-content").css('border-bottom-right-radius','15px');
+	});
 </script>
